f106ba3b7e3e6a482b084c0c81f8508b73c3c823
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2022-03-09 21:25:13 +0500

    Alexa Music 2.0 With Yukki

--- YukkiMusic/platforms/Telegram.py
+++ YukkiMusic/platforms/Telegram.py
@@ -1,11 +1,12 @@
 #
 # Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
+# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+
+# Kanged By © @Dr_Asad_Ali
+# Rocks © @Shayri_Music_Lovers
+# Owner Asad Ali 
+# Harshit Sharma
+# All rights reserved. Yukki
 
 import os
 import time
@@ -16,7 +17,7 @@ from pyrogram.types import Voice
 
 import config
 from config import MUSIC_BOT_NAME
-from YukkiMusic import app
+from AasthaMusicBot import app
 
 from ..utils.formatters import (convert_bytes, get_readable_time,
                                 seconds_to_min)
