acfba40c61e9ced96674f6495f03ca8e6b035dfe
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2022-03-10 00:30:03 +0500

    Alexa Music 2.0 With Yukki

--- AasthaMusicBot/core/bot.py
+++ AasthaMusicBot/core/bot.py
@@ -17,11 +17,11 @@ import config
 from ..logging import LOGGER
 
 
-class YukkiBot(Client):
+class AlexaBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
-            "AasthaMusic",
+            "AasthaMusicBot",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
