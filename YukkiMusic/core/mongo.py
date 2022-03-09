896d54faf4e531043e9cb37e087d3ddb3a521519
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2022-03-09 21:24:25 +0500

    Alexa Music 2.0 With Yukki

--- YukkiMusic/core/mongo.py
+++ YukkiMusic/core/mongo.py
@@ -1,11 +1,12 @@
 #
 # Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
+# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+
+# Kanged By © @Dr_Asad_Ali
+# Rocks © @Shayri_Music_Lovers
+# Owner Asad Ali 
+# Harshit Sharma
+# All rights reserved. Yukki
 
 from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
 from pymongo import MongoClient
@@ -15,5 +16,5 @@ import config
 _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
 _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
 
-mongodb = _mongo_async_.Yukki
-pymongodb = _mongo_sync_.Yukki
+mongodb = _mongo_async_.Alexa
+pymongodb = _mongo_sync_.Alexa
