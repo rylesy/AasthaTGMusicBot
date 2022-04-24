b0188228e407195e28b66cfbf1da5a17ea3e97a9
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2022-04-24 17:10:11 +0500

    Error Fixing

--- AasthaMusicBot/__init__.py
+++ AasthaMusicBot/__init__.py
@@ -11,7 +11,6 @@
 from AasthaMusicBot.core.bot import AlexaBot
 from AasthaMusicBot.core.dir import dirr
 from AasthaMusicBot.core.git import git
-from AasthaMusicBot.core.userbot import Userbot
 from AasthaMusicBot.misc import dbb, heroku, sudo
 
 from .logging import LOGGER
